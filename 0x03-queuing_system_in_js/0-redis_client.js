4e7f72c5bf3880d1dfc6e29664daf3f5c6116e13
mainangaruiya <003paulmaina@gmail.com>
2023-11-27 11:37:50 +0300

    Redis client connected to the server when the connection to Redis works correctly It should log to the console the message Redis client not connected to the server: ERROR_MESSAGE when the connection to Redis does not work

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -1,10 +1,13 @@
 import {createClient, print } from 'redis';
- 
-const client = createClient();
-client.on('connect', function(){
-	console.log('Redis client connected to the server');
-});
 
-client.on('error', function (err) {
-  console.log(`Redis client not connected to the server: ${err}`);
-});
+function redisConnect(){
+	const client = createClient();
+
+	client.on('connect', function(){
+		console.log('Redis client connected to the server');
+		}).on('error', (err) => {
+    console.log(`Redis client not connected to the server: ${err}`);
+  });
+
+};
+redisConnect();
